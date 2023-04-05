800683eb6ebb4a6cb5940ea5bd93197e9ef50e41
Tatiana Osipova <otv120780@gmail.com>
2023-04-05 09:56:01 -0400

    feat: add file column js

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ canvas.height = window.innerHeight;
 const FONT_SIZE = 16;
 
 contex.font = `bold ${FONT_SIZE}px monospace`;
-contex.fillStyle = 'green';
 
 let y = 0;
 
 function animate() {
+    contex.fillStyle = 'black';
+    contex.fillRect(0, 0, canvas.width, canvas.height);
+
+    contex.fillStyle = 'green';
+
     contex.fillText('M', 100, y);
     y += FONT_SIZE;
 
