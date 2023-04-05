2442b5c8f7549295916f4453285aaa8da273a3db
Tatiana Osipova <otv120780@gmail.com>
2023-04-05 09:48:20 -0400

    feat: add font-size

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const contex = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-contex.font = 'bold 16px monospace';
+const FONT_SIZE = 16;
+
+contex.font = `bold ${FONT_SIZE}px monospace`;
 contex.fillStyle = 'green';
 
 contex.fillText('M', 100, 100);
+contex.fillText('M', 100, 100 + 16);
