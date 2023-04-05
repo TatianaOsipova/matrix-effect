b630d2b3d3b35835e2cf13b952ccdd00ac965e49
Tatiana Osipova <otv120780@gmail.com>
2023-04-05 11:17:49 -0400

    feat: correct code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { Column } from "./column.js";
+
 const canvas = document.getElementById('canvas');
 const contex = canvas.getContext('2d');
 
@@ -6,18 +8,20 @@ canvas.height = window.innerHeight;
 
 const FONT_SIZE = 16;
 
-contex.font = `bold ${FONT_SIZE}px monospace`;
+context.font = `bold ${FONT_SIZE}px monospace`;
 
-let y = 0;
+const column = new Column(100, FONT_SIZE, canvas.height, context);
 
 function animate() {
-    contex.fillStyle = 'black';
-    contex.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-    contex.fillStyle = 'green';
+    context.fillStyle = 'green';
+    column.drawSymbol();
 
-    contex.fillText('M', 100, y);
-    y += FONT_SIZE;
+
+    // context.fillText('M', 100, y);
+    // y += FONT_SIZE;
 
     setTimeout(() => requestAnimationFrame(animate), 50);
 }
